ec72d7a447af34ade6af877b3f75426f18c4ad1f
xuyi26 <31821345+xuyi26@users.noreply.github.com>
2018-04-17 23:57:56 +0800

    Create UFODate.cs

---
+++ UFODate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFODate : MonoBehaviour {
+    public int score = 1;
+    public Color color = Color.blue;
+    public Vector3 direction;
+    public Vector3 scale = new Vector3(2, 0.3f, 2);
+}
